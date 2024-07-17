01c3fe4f8842b3ee8e3e0a358bd9f06dc01097ed
muna <munapm1@gmail.com>
2024-07-17 13:37:29 +0400

    changes in settings

--- finsta/finsta/settings.py
+++ finsta/finsta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-203s%@_shtukdna40ulmcx#)dz7%x$zo)1c1hef38l*6w3*=%_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
